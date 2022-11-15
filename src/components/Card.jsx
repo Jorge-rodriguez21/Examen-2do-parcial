c56896af363f1c2c80eee2a59f54e1b153822f2d
Jorge-rodriguez21 <114625381+Jorge-rodriguez21@users.noreply.github.com>
2022-11-14 22:43:05 -0600

    Proyecto Examen 2do Parcial-AWP

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,16 @@ import {Link} from 'react-router-dom'
 const Card = ({nombre,apellidos, twitter, email,id}) => {
    
     const path = `/assets/usuario.png`
+    const pathTwitter = `/assets/twitter.png`
 
     return (
         <div className="card m-3 col-12 col-md-4" style={{width: 400}}>
-            <img className="card-img-top" width="5%" height="5%" src={path}/>
+            <br/>
+            <img src={path} width="25%" height="25%" alt="animacion"/>
             <div className="card-body">
                 <h3 className="card-title">{nombre} {apellidos}</h3>
                 <p className="card-text">
+                    <img src={pathTwitter} width="10%" height="10%" alt="animacion"/>
                     <a href={`https://twitter.com/@${twitter}`}>{twitter}</a>
                     <br />
                     <Link className="btn btn-outline-primary" to={`/modificarScreen/${id}`}>
